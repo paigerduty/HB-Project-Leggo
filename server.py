a974e70259087495593daf612e6d53a5fbdabfd4
Paige <bernier.paige@gmail.com>
2016-08-10 23:12:57 +0000

    Got yelp API call working with hard coded values TODO dynamically retrieve from user form submission.

--- server.py
+++ server.py
@@ -3,11 +3,13 @@ import os
 import requests
 
 # Importing from pip installed libraries
-from rauth import OAuth2Service
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
 from model import connect_to_db, db
+
 
 app = Flask(__name__)
 
@@ -17,18 +19,6 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 # Raises an error in Jinja when an undefined var is used
 app.jinja_env.undefined = StrictUndefined
-
-# Sets up a service wrapper to obtain an access token 
-# after the authorization URL has been visited by the client
-
-yelp = OAuth2Service(
-	client_id = os.environ.get'YELP_CONSUMER_KEY'
-	client_secret = os.environ.get'YELP_CONSUMER_SECRET'
-	name='yelp'
-	authorize_url=''
-	token
-	token_secret
-
 
 @app.route('/')
 def index():
@@ -46,18 +36,43 @@ def submit_data():
 	latitude = request.form['latitude']
 	longitude = request.form['longitude']
 
-	# Changes distance radius from miles to meters for Yelp API call
-	# Limits to 2 decimal points
-	 distance_radius_m = float("{0:2f}".format(int(distance_radius) * 1609.34))
+	# Need to create a geographical bounding box to give to Yelp 
+	# This will be based on the starting point +/- the given radius 
+	# in all directions ne, nw, se, sw
 
-	# Store these in the session so I can use to make API call
-	session['time_pref'] = time_pref
-	session['distance_radius'] = distance_radius
-	session['latitude'] = latitude
-	session['longitude'] = longitude
+	# Changes distance radius from miles to meters for Yelp API call
+	distance_radius_m = int(distance_radius) * 1609.34
+
+	# Creates a bounding box (4 coordinates ne, nw, se, sw)
+
 
 	# Return JSON 
 	return "Success"
+
+@app.route('/yelp_call')
+def call_yelp():
+
+	# Sets up Oauth for Yelp API requests
+	auth = Oauth1Authenticator(
+		consumer_key = os.environ.get('YELP_CONSUMER_KEY'),
+		consumer_secret = os.environ.get('YELP_CONSUMER_SECRET'),
+		token = os.environ.get('TOKEN'),
+		token_secret = os.environ.get('TOKEN_SECRET')
+	)
+
+	# Binds a Client object giving it authentication credentials
+	client = Client(auth)
+
+	# Creates dict params based on Yelp Search parameters
+	params = {
+		'term':'food',
+		'sort': 2,
+		'location':'San Francisco'
+	}
+
+	# Calls Yelp API with parameters specified information
+	# ** allows an arbitrary amount of arguments to be passed
+	response = client.search(**params)
 
 if __name__ == '__main__':
 	app.debug = True
--- yelp_yum_results.py
+++ yelp_yum_results.py
@@ -1,14 +1,39 @@
-from yelpapi import YelpAPI
-import pprint
 import os
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
 
-# Accesses keys & tokens from Yelp that are securely set in environment
-consumer_key = os.environ['YELP_CONSUMER_KEY']
-consumer_secret = os.environ['YELP_CONSUMER_SECRET']
-token = os.environ['YELP_ACCESS_TOKEN_KEY']
-token_secret = os.environ['YELP_ACCESS_TOKEN_SECRET']
 
-yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
+def call_yelp():
+
+	# Sets up Oauth for Yelp API requests
+	auth = Oauth1Authenticator(
+		consumer_key = os.environ.get('YELP_CONSUMER_KEY'),
+		consumer_secret = os.environ.get('YELP_CONSUMER_SECRET'),
+		token = os.environ.get('TOKEN'),
+		token_secret = os.environ.get('TOKEN_SECRET')
+	)
+
+	# Binds a Client object giving it authentication credentials
+	client = Client(auth)
+
+	# Creates dict params based on Yelp Search parameters
+	# How to dynamically get from form submission
+	params = {
+		'term':'food',
+		'sort': 2,
+		'location':'San Francisco'
+	}
+
+	# Calls Yelp API with parameters specified information
+	# ** allows an arbitrary amount of arguments to be passed
+	response = client.search(**params)
+
+	# Hold all objects in a list
+	# Choose the first
+	# Hold the first and get the attributes that the app wants
+
+	return response
+
 
 # Give yelp the lat/long and time preference from user form submit
 
