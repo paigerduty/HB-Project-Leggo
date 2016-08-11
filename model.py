418b9c91bb4138a3c7065e5a975c30ac698a64d5
Paige <bernier.paige@gmail.com>
2016-08-11 01:21:17 +0000

    Further populated model.py with class attributes.

--- model.py
+++ model.py
@@ -8,31 +8,70 @@ db = SQLAlchemy()
 ####################################################################
 # Model definitions
 
-class User(db.Model):
-	'''Defines a User object that interacts with app.'''
-	pass
+# db.Model is a base class for all Models for SQLAlchemy
+# class User(db.Model):
+# 	'''Defines a User object that interacts with app.'''
+# 	# Needs to go to SQLAlchemy
+# 	__tablename__ = "users"
+# 	# Contains likes/dislikes of adventures and/or yums yays
+# 	# Contains password
+# 	# Contains their 'genre'
+
+# 	# email & pass needed to instantiate User
+# 	def __init__(self,email,password):
+# 		self.email = email
+# 		self.password = password
+
+# 	# when object called, show helpful info
+# 	def __repr__(self):
+# 		return '<User %r>' % self.email
+# 	pass
 
 class Adventure(db.Model):
 	'''Defines an Adventure object (1 Yay & 1 Yum)'''
+	__tablename__ = "adventures"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	# yum_possibilities is a list of ids 
+	# yay_possibilities is a list of ids
+	# Contains yum_possibilities and yay_possibilities list
+	# Contains a collection of yum_possibilities and yay_possibilites
+	# Contains a function to calculate all the possibilites
+	# Contains a function to hold lists of possibilities and 'pop' them
 	pass
 
 class Component(db.Model):
 	'''Defines unique attributes of a part of Adventure.'''
-	pass
+	
+	# Not the best table name plz fix
+	__tablename__ = "components"
 
-class Yay(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+	time = db.Column(db.Time, nullable=False)
+	lat = db.Column(db.Integer(8), nullable=False)
+	lng = db.Column(db.Integer(8), nullable=False)
+	name = db.Column(db.String(50), nullable=False)
+	desc = db.Column(db.String(300), nullable=False)
+	url = db.Column(db.String(200), nullable=False)
+	genre = db.Column(db.String(20), nullable=True)
+	time_bucket = db.Column(db.Integer(1), nullable=False)
+	
+	# Define relationship between Component and Yay
+	# Define relationship bewteen Component and Yum
+	# Backref them!!1
+	
+class Yay(Component):
 	''' Defines a Yay object (1 restaurant/bar/snack). Child of Component'''
-
 	__tablename__ = "yays"
 
-	yay_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	name = db.Column(db.String(100), nullable=False)
-	description = db.Column(db.String(500), nullable=False)
-	url = db.Column(db.String(500), nullable=True)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-class Yum(db.Model):
+
+class Yum(Component):
 	'''Defines a Yum object (1 activity). Child of Component.'''
-	pass
+	__tablename__ = "yums"
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	
 
 ####################################################################
 # Helper functions
@@ -42,6 +81,7 @@ def connect_to_db(app):
 
 	#Configure to use PostgreSQL database
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///yays'
+	app.config['SQLALCHEMY_ECHO'] = True
 	db.app = app
 	db.init_app(app)
 
