b39f402528c18ec333683b8e6fc1233531fde7e1
Paige <bernier.paige@gmail.com>
2016-08-17 16:31:12 +0000

    Refined SFWeekly Spider object to scrape the name and url of event. WIP. TODO recursively search to get the address from page 2 and instantiate Yay object and add to DB.

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -12,4 +12,5 @@ class SFWeeklyItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     url = scrapy.Field()
+    location = scrapy.Field()
     
--- tutorial/tutorial/spiders/sfweekly_spider.py
+++ tutorial/tutorial/spiders/sfweekly_spider.py
@@ -1,13 +1,21 @@
 import scrapy
 
-class YayObject(scrapy.Spider):
-	name = 'sfweekly'
-	allowed_domains = ['sfweekly.com']
-	start_urls = ['http://archives.sfweekly.com/sanfrancisco/EventSearch?narrowByDate=Today']
+class SFWeeklySpider(scrapy.Spider):
+	name = "sfweekly"
+	allowed_domains = ["sfweekly.com"]
+	start_urls = ["http://archives.sfweekly.com/sanfrancisco/EventSearch?narrowByDate=Today"]
 
 	def parse(self,response):
 		""" For each 'event' grab the name and the url."""
-		for sel in response.xpath
+
+		filename = response.url.split("/")[-2] + '.html'
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+
+		for sel in response.xpath('//*[@id="searchResults"/div/div/div'):
+			name = sel.xpath('a/span').extract()
+			url = sel.xpath('a/@href').extract()
+			# Location! Tell Scrapy to recursively scrape the event page 
 
 	def parse_yay(self, response):
 		""" Follows the url to grab the element 'span.street-address' """
