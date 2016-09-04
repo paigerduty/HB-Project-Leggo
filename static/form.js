51b5e3bd8562f802bdac75fc6a815a68dfa97c23
Paige <bernier.paige@gmail.com>
2016-09-04 21:44:57 +0000

    Wrote js to hide Submit button until latitude and longitude load

--- static/form.js
+++ static/form.js
@@ -4,7 +4,6 @@ var latitude;
 var longitude;
 var latLng;
 var markers;
-var map;
 var yay_lat;
 var yay_long;
 // var markerCount = 0;
@@ -18,6 +17,8 @@ function handleGetCurrentPosition(location){
 	longitude = document.getElementById("longitude");
 	latitude.value = location.coords.latitude;
 	longitude.value = location.coords.longitude;
+
+	$('#btn').attr("style","");
 
 };
 
@@ -76,9 +77,11 @@ function getAdventure(result){
 	$('#swap-yum').attr("style", "");
 };
 
+var map;
+var markers;
 function initMap(current_yum, current_yay){
 	console.log("initMap called");
-	var map;
+	// var map;
 	// Makes new map Object
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: parseFloat(latitude.value), lng: parseFloat(longitude.value)},
@@ -90,7 +93,7 @@ function initMap(current_yum, current_yay){
 	yay_latitude = loc[1];
 	yay_longitude = loc[3];
 
-	var markers = [
+	markers = [
 		['user_marker', parseFloat(latitude.value),parseFloat(longitude.value),1],
 		['yay_marker', parseFloat(yay_latitude),parseFloat(yay_longitude),2],
 		['yum_marker', current_yum.latitude, current_yum.longitude,3]
@@ -119,40 +122,42 @@ function submitSwapYay(evt){
 	// debugger;
 
 	var geocoder = new google.maps.Geocoder();
-	var loc = current_yay.location;
 
-	geocoder.geocode( { 'address': loc}, function(results, status) {
-	    var yay_lat = results[0].geometry.location.lat();
-	    console.log(yay_lat);
-	    var yay_long = results[0].geometry.location.lng();
-	    console.log(yay_long);
+	var yay_lat;
+	var yay_long;
+	geocoder.geocode({'address': current_yay.location}, function(results) {
+	    console.log("results", results)
+	    yay_lat = results[0].geometry.location.lat();
+	    // debugger;
+	    console.log("yay_lat",yay_lat);
+	    yay_long = results[0].geometry.location.lng();
+	    console.log("yay_long",yay_long);
 	}); 
 
 
-	// loc = loc.replace(",",":").slice(2,-2).split(":");
-	// yay_latitude = loc[1];
-	// yay_longitude = loc[3];
-	console.log(">>>>>>>>>>>>>>")
-	console.log("geocoded addy")
+	console.log(">>>>>>>>>>>>>>");
+	console.log("geocoded addy");
 
-	var yay_marker = new google.maps.Marker({
-		position: {lat: parseFloat(yay_lat), 
-			       lng: parseFloat(yay_long)},
-		map:map
-	});
+	// ONCE MARKER IS ADDED TURN INTO A FUNCTION THEN CALL THAT FXN AND PASS LATLNG
+	console.log("YAY INFO INCOMING");
+	console.log(markers[1]);
+	console.log(markers[1][1]);
+
+	var cur_yay = markers[1];
+	console.log("cur_yay",cur_yay);
+	debugger;
+	cur_yay[1] = yay_lat;
+	cur_yay[2] = yay_long;
+
+	console.log("HOPEFULLY STUFF CHANGED");
+	console.log("cur_yay", cur_yay);
+
+	var new_marker = new google.maps.Marker({
+			position: {lat: markers[i][1], lng: markers[i][2]},
+			map:map
+		});
 
 	console.log("should have placed new yay marker")
-	// var markers = [
-	// 	['user_marker', parseFloat(latitude.value),parseFloat(longitude.value),1],
-	// 	['yay_marker', parseFloat(yay_latitude),parseFloat(yay_longitude),2],
-	// 	['yum_marker', current_yum.latitude, current_yum.longitude,3]
-	// ];
-
-	// for (i=0; i<markers.length;i++){
-	// 	marker = new google.maps.Marker({
-	// 		position: {lat: markers[i][1], lng: markers[i][2]},
-	// 		map:map
-	// 	});
 }
 
 
@@ -168,6 +173,33 @@ function submitSwapYum(evt){
 	$("#yum_url").attr("href", current_yum.url);
 	$("#yum_name").html(current_yum.name);
 	$("#yum_location").html(current_yum.location);
+
+
+	console.log("new yum",markers[2]);
+	console.log("new yum lat",markers[2][1]);
+
+
+	var loc = current_yum.location;
+	loc = loc.replace(",",":").slice(2,-2).split(":");
+	yum_latitude = loc[1];
+	yum_longitude = loc[3];
+
+	var cur_yum = markers[2];
+	console.log("cur_yum",cur_yum);
+	console.log("Look at methods on current_yum");
+	debugger;
+	cur_yum[1] = yum_latitude;
+	cur_yum[2] = yum_longitude;
+
+	console.log("HOPEFULLY STUFF CHANGED");
+	console.log("cur_yay", cur_yay);
+
+	var new_marker = new google.maps.Marker({
+			position: {lat: cur_yum[1], lng: cur_yum[2]},
+			map:map
+		});
+
+	console.log("should have placed new yay marker")
 }
 
 $('#swap-yum').on("click", function(evt){
